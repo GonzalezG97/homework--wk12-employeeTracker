08fa21669c6303b35fac3e08ae1a492ce1a0704b
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-04-13 20:53:47 -0500

    Added a go back feature

--- app.js
+++ app.js
@@ -1,94 +1,107 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-// Database connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 8080,
-  user: "root",
-  password: "",
-  database: "",
-});
+// // Database connection
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 8080,
+//   user: "root",
+//   password: "",
+//   database: "",
+// });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log(`connected as id ${connection.threadId}`);
-  mainMenu();
-});
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log(`connected as id ${connection.threadId}`);
+//   mainMenu();
+// });
+
+
 
 // Prompts
 const mainMenu = () => {
-    inquirer
-        .prompt([
-            {
-            type: 'list',
-            message: 'What do you want to do?',
-            name: 'direction',
-            choices: ['Add new', 'View current', 'Update roles']
-            },
-        ]).then(answer => {
-            switch (answer.direction) {
-                case 'Add new':
-                    addMenu();
-                    break;
-                case 'View current':
-                    viewMenu();
-                    break;
-                case 'Update roles':
-                    updateRoles();
-                    break;                
-            }
-        });
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What do you want to do?",
+        name: "direction",
+        choices: ["Add new", "View current", "Update roles"],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.direction) {
+        case "Add new":
+          addMenu();
+          break;
+        case "View current":
+          viewMenu();
+          break;
+        case "Update roles":
+          updateRoles();
+          break;
+      }
+    });
 };
 
 const addMenu = () => {
-    inquirer
-        .prompt([
-            {
-            type: 'list',
-            message: 'What do you want to add?',
-            name: 'addWhat',
-            choices: ['Add new Department', 'Add new Roles', 'Add employee']
-            },
-        ]).then(answers => {
-            switch (answers.addWhat) {
-                case 'Add new Department':
-                  addDepartment();
-                  break;
-                case 'Add new Roles':
-                  addRoles();
-                  break;
-                case 'Add employee':
-                  addEmployee();
-                  break;
-              }
-        })
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What do you want to add?",
+        name: "addWhat",
+        choices: ["Add new Department", "Add new Roles", "Add employee", "Go back"],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.addWhat) {
+        case "Add new Department":
+          addDepartment();
+          break;
+        case "Add new Roles":
+          addRoles();
+          break;
+        case "Add employee":
+          addEmployee();
+          break;
+        case "Go Back":
+            mainMenu();
+            break;
+      }
+    });
 };
 
 const viewMenu = () => {
-    inquirer
-        .prompt([
-            {
-            type: 'list',
-            message: 'What do you want to view?',
-            name: 'view',
-            choices: ['View Departments', 'View Roles', 'View Employee']
-            },
-        ]).then(answer => {
-            switch (answer.view {
-                case 'View Departments':
-                    viewDepartments();
-                    break;
-                case 'View Roles':
-                    viewRoles();
-                    break;
-                case 'View Employee':
-                    viewEmployee();
-                    break;                
-            }
-        });
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What do you want to view?",
+        name: "view",
+        choices: ["View Departments", "View Roles", "View Employee", "Go Back"],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.view) {
+        case "View Departments":
+          viewDepartments();
+          break;
+        case "View Roles":
+          viewRoles();
+          break;
+        case "View Employee":
+          viewEmployee();
+          break;
+        case "Go Back":
+            mainMenu();
+            break;
+      }
+    });
 };
 
 const updateRoles = () => {
-
+    
 };
+
+mainMenu();
